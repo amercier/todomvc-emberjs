676c1636938f2725007798c3cf8401f07be977d2
Alexandre Mercier <amercier@slideshare.com>
2014-10-31 17:03:01 -0700

    Fixed gulp-useref API changes

--- gulpfile.js
+++ gulpfile.js
@@ -27,16 +27,17 @@ gulp.task('scripts', function () {
 gulp.task('html', ['styles', 'scripts'], function () {
   var jsFilter = $.filter('**/*.js');
   var cssFilter = $.filter('**/*.css');
+  var assets = $.useref.assets({searchPath: '{.tmp,app}'});
 
   return gulp.src('app/*.html')
-    .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
+    .pipe(assets)
     .pipe(jsFilter)
     .pipe($.uglify())
     .pipe(jsFilter.restore())
     .pipe(cssFilter)
     .pipe($.csso())
     .pipe(cssFilter.restore())
-    .pipe($.useref.restore())
+    .pipe(assets.restore())
     .pipe($.useref())
     .pipe(gulp.dest('dist'))
     .pipe($.size());
